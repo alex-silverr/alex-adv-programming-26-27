8e14a85d8814180ee4387c95e48ccba551388840
Alex <alexpersiv@gmail.com>
2026-07-09 16:09:37 +0100

    I forgot to actually add the path..

--- src/app.py
+++ src/app.py
@@ -44,6 +44,7 @@ class ShowThings(Resource):
 
 api.add_resource(Index, "/")
 api.add_resource(ShowThings, "/things")
+api.add_resource(MakeThings, "/newthing")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port='8080', debug=True)
