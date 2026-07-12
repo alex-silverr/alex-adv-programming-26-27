528d5af146557edc45b390e95b87f80e30033f69
Alex <alexpersiv@gmail.com>
2026-07-12 01:54:38 +0100

    fix: cont. fixing option edit

--- src/app.py
+++ src/app.py
@@ -333,8 +333,8 @@ api.add_resource(MakeTicketStatus, "/tstatus", methods=['GET', 'POST'])
 api.add_resource(MakeEventType, "/evtype", methods=['GET', 'POST'])
 api.add_resource(MakeEditedField, "/edfield", methods=['GET', 'POST'])
 api.add_resource(MakeUserRole, "/urole", methods=['GET', 'POST'])
-api.add_resource(MakeEditedField, "/eedfield/<int:index>", methods=['PUT'])
-api.add_resource(MakeUserRole, "/eurole/<int:index>", methods=['PUT'])
+api.add_resource(EditEditedFields, "/eedfield/<int:index>", methods=['PUT'])
+api.add_resource(EditUserRole, "/eurole/<int:index>", methods=['PUT'])
 
 
 
