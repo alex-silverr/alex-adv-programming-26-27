c89f3be6dc663e7da5191faa20f01b6b749c8f62
Alex <alexpersiv@gmail.com>
2026-07-14 21:04:38 +0100

    test/fix: remove user from ticket debug 3

--- src/connect/ticket/ticket_update.py
+++ src/connect/ticket/ticket_update.py
@@ -54,9 +54,9 @@ def removeUserAssignment(id, args={}):
     
     with Session(dbeng) as session:
         if "assign_user_id" in args:
-            user = ticket.assigned_to_user.select(User).where(
-                User.id == args.get("assign_user_id")
-            )
+            user = [user for user in 
+                    ticket.assigned_to_user if
+                    user.id == args.get("assign_user_id")]
         else:
             raise ValueError("User to assign ticket to not given")
         current_app.logger.debug("ping")
