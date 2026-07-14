5a3827ff6e2241956c39a71585ad5a4839abf96f
Alex <alexpersiv@gmail.com>
2026-07-14 17:05:59 +0100

    test: user update debug 1

--- src/connect/user/user_update.py
+++ src/connect/user/user_update.py
@@ -1,4 +1,6 @@
 from sqlalchemy.orm import Session
+from flask import current_app
+
 from src import dbeng
 import src.connect as ct
 
@@ -7,10 +9,15 @@ def updateInfoUser(id, args={}):
     User UPDATE - aux:
     Update User info: display name, full name, e-mail, github, role
     """
+    current_app.logger.debug("ping")
     id = int(id)
     user = ct.getUser(id)
 
+    current_app.logger.debug("pong")
+    current_app.logger.debug(user)
+
     with Session(dbeng) as session:
+        current_app.logger.debug("bing")
         if "display_name" in args: user.display_name = args.get("display_name")
         if "full_name" in args: user.full_name = args.get("full_name")
         if "email" in args: user.email = args.get("email")
@@ -19,6 +26,9 @@ def updateInfoUser(id, args={}):
             role = ct.optionGetById("user_role", args.get("role_id"))
             if not role: raise ValueError("Could not update user: invalid role")
             role.r_role = role
+        current_app.logger.debug("bong")
+        current_app.logget.debug(user)
 
+        session.merge(user)
         session.commit()
     return user
