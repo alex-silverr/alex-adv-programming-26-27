b32ec34d19d73bd0030d175a11f37308a0152407
Alex <alexpersiv@gmail.com>
2026-07-14 17:10:06 +0100

    test: user update debug - removing pings

--- src/connect/user/user_update.py
+++ src/connect/user/user_update.py
@@ -9,15 +9,10 @@ def updateInfoUser(id, args={}):
     User UPDATE - aux:
     Update User info: display name, full name, e-mail, github, role
     """
-    current_app.logger.debug("ping")
     id = int(id)
     user = ct.getUser(id)
 
-    current_app.logger.debug("pong")
-    current_app.logger.debug(user)
-
     with Session(dbeng) as session:
-        current_app.logger.debug("bing")
         if "display_name" in args: user.display_name = args.get("display_name")
         if "full_name" in args: user.full_name = args.get("full_name")
         if "email" in args: user.email = args.get("email")
@@ -26,8 +21,6 @@ def updateInfoUser(id, args={}):
             role = ct.optionGetById("user_role", args.get("role_id"))
             if not role: raise ValueError("Could not update user: invalid role")
             role.r_role = role
-        current_app.logger.debug("bong")
-        current_app.logger.debug(user)
 
         session.merge(user)
         session.commit()
