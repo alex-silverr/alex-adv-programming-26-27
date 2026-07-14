04c49cf92f926dbac935bd2467b8a1c75b6a8486
Alex <alexpersiv@gmail.com>
2026-07-14 02:34:40 +0100

    ???

--- src/database.py
+++ src/database.py
@@ -3,7 +3,6 @@ from typing import List
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
 from src.settings import SQLALCHEMY_DATABASE_URL
-import src.connect as ct
 
 # -------------------------------
 # Base from https://docs.sqlalchemy.org/en/20/orm/quickstart.html
